5e47ea54dd362fffd5c4a640d63d5ed84745d7fa
GAssadRocha <gassadrocha@gmail.com>
2023-03-07 23:23:40 -0500

    Created 2 database: Transaction and User_info

--- Owe_app/admin.py
+++ Owe_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from Owe_app.models import UserInfo, Transaction
 
 # Register your models here.
+
+admin.site.register(UserInfo)
+admin.site.register(Transaction)
--- Owe_app/models.py
+++ Owe_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class UserInfo(models.Model):
+    first_name = models.CharField(max_length=30, primary_key=True)
+    last_name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.first_name
+
+class Transaction(models.Model):
+    name = models.CharField(max_length=30, primary_key=True)
+    payer_user_name = models.ForeignKey(UserInfo, on_delete=models.DO_NOTHING)
+    payee_user_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+
